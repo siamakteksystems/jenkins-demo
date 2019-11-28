33fba4b2a37328b2466fa8eb59b1e7cee497b470
Siamak Sedehi Eshghi <siamak_sedehi_eshghi@bat.com>
2019-11-28 14:37:05 -0500

    test

--- index.js
+++ index.js
@@ -9,4 +9,5 @@ app.get('/', function(req, res) {
 const port = process.env.PORT || 4000;
 const server = app.listen(port, () => console.info(`Listening on port ${port}...`));
 
-module.exports = server;
+
+module.exports = server; // added test
